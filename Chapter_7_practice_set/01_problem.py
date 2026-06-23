9b102c92716bab4bb7afd16fb0c792e06ffecd15
Sudarshan singh <sudarshansingh9009@gmail.com>
2026-06-23 21:03:38 +0530

    Started chapter 7 practice questions

---
+++ Chapter_7_practice_set/01_problem.py
@@ -0,0 +1,9 @@
+# Write a program to print multiplication table of a given number using for loop
+
+for i in range (1, 101):
+     if( i % 2 == 0):
+          print(i)
+          
+     else:
+          continue
+
